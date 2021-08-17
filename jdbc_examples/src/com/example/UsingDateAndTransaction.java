0d185fc930fc67e4b1929a08e237db5052c01e5f
srivatsan <vatsan_k@hotmail.com>
2021-08-17 17:38:33 +0530

    update

--- jdbc_examples/src/com/example/UsingDateAndTransaction.java
+++ jdbc_examples/src/com/example/UsingDateAndTransaction.java
@@ -8,37 +8,21 @@ public class UsingDateAndTransaction {
 
 	public static void main(String[] args) {
 
+		Savepoint sp1=null;
+		Savepoint sp2=null;
+		
 	
+		Connection con=null;
 		try {
 			
-			
+	
+			 
 		
-			Connection con = DbConnection.getMySqlConnection();
-			
-			String sqlOne = "insert into department values(?,?,?)";
-			
-			PreparedStatement pstmt2One = con.prepareStatement(sqlOne);
+			 con = DbConnection.getMySqlConnection();
 			
 			
-			 LocalDate date = LocalDate.of(2021, 8, 17);
-			
-	     
-		     Date sqlDate = Date.valueOf(date);
-		     
-		     System.out.println(sqlDate);
-			
-			pstmt2One.setInt(1,202);
-			pstmt2One.setString(2, "ECE");
-			pstmt2One.setDate(3,sqlDate );
-			
-			
-			int rowAdded =pstmt2One.executeUpdate();
-			
-			System.out.println("Row Added :="+ rowAdded);
-			
-			
-			// next query
-			
+			con.setAutoCommit(false);
+						
 			String sqlTwo ="insert into student values(?,?,?)";
 			
 			
@@ -53,13 +37,49 @@ public class UsingDateAndTransaction {
 					pstmt2.setDouble(3, 87);
 
 
-					rowCount = pstmt2.executeUpdate();
+					rowCount = pstmt2.executeUpdate();  // insert row => undo insert row
 
 					System.out.println(rowCount);
-			
+		
+					 sp1 =con.setSavepoint("point1");  // savepoint
+					 
+					// next query
+					
+					String sqlOne = "insert into department values(?,?,?)";
+					
+					PreparedStatement pstmtOne = con.prepareStatement(sqlOne);
+					
+					
+					 LocalDate date = LocalDate.of(2021, 8, 17);
+					
+			     
+				     Date sqlDate = Date.valueOf(date);
+				     
+				     System.out.println(sqlDate);
+					
+					pstmtOne.setInt(1,202);
+					pstmtOne.setString(2, "ECE");
+					pstmtOne.setDate(3,sqlDate );
+					
+					
+					int rowAdded =pstmtOne.executeUpdate();
+					
+					System.out.println("Row Added :="+ rowAdded);
+					
+					sp2=con.setSavepoint("point2");
+					
+					con.commit();
+					
+				
 			
 		} catch (Exception e) {
 			
+			try {
+				con.rollback(sp2);
+			} catch (SQLException e1) {
+				e1.printStackTrace();
+			}
+			  
 			 e.printStackTrace();
 		}
 		
